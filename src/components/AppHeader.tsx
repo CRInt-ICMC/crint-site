235e58d0f8ee5be4dc11617aa1ce24b4eede8bc9
CRInt-ICMC <136650250+CRInt-ICMC@users.noreply.github.com>
2023-06-21 17:43:47 -0300

    Atualizações na barra de navegação

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,19 +1,19 @@
-// Componentes
+// COMPONENTES
 import { useLocation } from 'react-router-dom'
 // CSS
 import './AppHeader.css'
-// Imagens
-import icmc from '../img/icmc-logo.png'
-import usp from '../img/usp-logo.png'
+// IMAGENS
+// import usp from '../img/usp-logo.png'
+import icmc from '../img/icmc-logo-completo.png'
 import crint from '../img/crint-logo.png'
 import en_flag from '../img/english-flag.png'
-import es_flag from '../img/spanish-flag.png'
+// import es_flag from '../img/spanish-flag.png'
 import { Link } from 'react-router-dom'
 
 const logos = () => {
     return (
     <span className='logos'>
-        <a href="https://www5.usp.br/"><img className='logo-usp' alt='' src={usp} /></a>
+        {/* <a href="https://www5.usp.br/"><img className='logo-usp' alt='' src={usp} /></a> */}
         <a href="https://www.icmc.usp.br/"><img className='logo-icmc' alt='' src={icmc} /></a>
         <Link to='/'><img className='logo-crint' alt='' src={crint} /></Link>
     </span>
@@ -24,36 +24,32 @@ const topics = () => {
     return (
         <span className='topics'>
             <div className='linha'>
-                <span> L1T1 </span>
-                <span> L1T2 </span>
-                <span> L1T3 </span>
-                <span> L1T4 </span>
-                <span> L1T5 </span>
+                <a href='http://localhost:3000'> GRADUAÇÃO </a>
+                <a href='http://localhost:3000'> PÓS-GRADUAÇÃO </a>
+                <a href='http://localhost:3000'> PESQUISA </a>
             </div>
             <div className='linha'>
-                <span> L2T1 </span>
-                <span> L2T2 </span>
-                <span> L2T3 </span>
-                <span> L2T4 </span>
-                <span> L2T5 </span>
+                <a href='http://localhost:3000'> CULTURA E EXTENSÃO </a>
+                <a href='http://localhost:3000'> INCLUSÃO E PERTENCIMENTO </a>
             </div>
         </span>
     );
 }
 
-const languages = () => {
-    // const location = useLocation();
-    // const currentURL = 
-    
+const languages = (currentURL : string) => {
     return (
         <span className='flags'>
-            <img className='en-flag' alt='' src={en_flag} />
-            <img className='es-flag' alt='' src={es_flag} />
+            <a href={currentURL + '?lang=en'}><img className='en-flag' alt='' src={en_flag} /></a>
+            {/* <a href={currentURL}><img className='es-flag' alt='' src={es_flag} /></a> */}
         </span>
     );
 }
 
 const AppHeader = () => {
+    // Pega a URL atual da página
+    const location = useLocation();
+    const currentURL = location.pathname;
+
     return (
         <header className='header-root'>
             <nav className='navbar'>
@@ -66,7 +62,7 @@ const AppHeader = () => {
                 </div>
 
                 <div className='navbar-right'>
-                    {languages()}
+                    {languages(currentURL)}
                 </div>
             </nav>
         </header>
