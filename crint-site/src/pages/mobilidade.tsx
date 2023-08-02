15e5bc0ddd6a0868fd669c720f5db3b90cf7773e
CRInt-ICMC <136650250+CRInt-ICMC@users.noreply.github.com>
2023-08-02 14:40:58 -0300

    Correção de Mobilidade

--- crint-site/src/pages/mobilidade.tsx
+++ crint-site/src/pages/mobilidade.tsx
@@ -1,26 +1,11 @@
 import TopicBanner from '../components/TopicBanner';
 import TopicSection from '../components/TopicSection';
 import './mobilidade.css';
+import WIP_page from './wip';
 
 const InterServidores = () => {
     return (
-        <>
-            <h3>Auxílios Financeiros</h3>
-            <p>
-                A CRInt tem apoiado os funcionários técnico-administrativos para a participação em eventos internacionais ou outros eventos 
-                pertinentes à internacionalização. A solicitação é em fluxo contínuo e deve ser feita diretamente pelo  
-                email <a href="mailto:crint@icmc.usp.br">crint@icmc.usp.br</a>. Recomenda-se que a solicitação seja feita com pelo menos 60 dias 
-                de antecedência do evento. 
-            </p>
-            <p>Documentos para a solicitação:</p>
-            <ul>
-                <li>Carta de aceite ou convite para o evento;</li>
-                <li>Plano de trabalho com as motivações, atividades a serem desenvolvidas, resultados esperados;</li>
-                <li>Orçamento dos valores sendo solicitados; e</li>
-                <li>Artigo a ser apresentado (se for o caso).</li>
-            </ul>
-
-            
+        <>            
             <h3>Oportunidades no Exterior e Editais em Andamento</h3>
             <p>
                 Ao longo do ano, diversas oportunidades de cursos de línguas, mobilidade internacional para servidores técnicos e administrativos e 
@@ -40,10 +25,7 @@ const Mobilidade = () => {
     return (
         <div id="mobilidade-root">
             <TopicBanner topicoNome='MOBILIDADE' />
-            <TopicSection
-                title='Internacionalização de Servidores Técnicos e Administrativos do ICMC/USP'
-                body={InterServidores()}
-                />
+            <WIP_page />
         </div>
     );
 }
--- crint-site/src/pages/mobility subpages/servidor.tsx
+++ crint-site/src/pages/mobility subpages/servidor.tsx
@@ -1,10 +1,33 @@
 import TopicBanner from '../../components/TopicBanner';
+import TopicSection from '../../components/TopicSection';
 import './servidor.css'
+
+const MobilidadeServidor = () => (
+    <>
+        <p>
+            A CRInt tem apoiado os funcionários técnico-administrativos para a participação em eventos internacionais ou outros eventos 
+            pertinentes à internacionalização. A solicitação é em fluxo contínuo e deve ser feita diretamente pelo  
+            email <a href="mailto:crint@icmc.usp.br">crint@icmc.usp.br</a>. Recomenda-se que a solicitação seja feita com pelo menos 60 dias 
+            de antecedência do evento. 
+        </p>
+        <p>Documentos para a solicitação:</p>
+        <ul>
+            <li>Carta de aceite ou convite para o evento;</li>
+            <li>Plano de trabalho com as motivações, atividades a serem desenvolvidas, resultados esperados;</li>
+            <li>Orçamento dos valores sendo solicitados; e</li>
+            <li>Artigo a ser apresentado (se for o caso).</li>
+        </ul>
+    </>
+)
 
 const Servidor = () => {
     return (
         <div id='servidor-root'>
             <TopicBanner topicoNome='SERVIDOR' />
+            <TopicSection
+                title='Mobilidade'
+                body={MobilidadeServidor()}
+                />
         </div>
     );
 }
