99bb47f4998105de2d2c45a487809b019d30528f
CRInt-ICMC <136650250+CRInt-ICMC@users.noreply.github.com>
2023-08-02 15:49:02 -0300

    WIP adicionados às páginas necessárias

--- crint-site/src/pages/information subpages/dia.tsx
+++ crint-site/src/pages/information subpages/dia.tsx
@@ -1,10 +1,12 @@
 import TopicBanner from '../../components/TopicBanner';
+import WIP_page from '../wip';
 import './dia.css'
 
 const Dia = () => {
     return (
         <div id='dia-root'>
             <TopicBanner topicoNome='DIA' />
+            <WIP_page />
         </div>
     );
 }
