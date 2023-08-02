94c9a0db89f5dcebea05df7db5fb09e3d8d52d79
CRInt-ICMC <136650250+CRInt-ICMC@users.noreply.github.com>
2023-08-02 11:20:42 -0300

    Atualizações gerais e preparação para lançamento

--- crint-site/src/components/AppFooter.tsx
+++ crint-site/src/components/AppFooter.tsx
@@ -1,18 +1,23 @@
-import './AppHeader.css'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { EMAIL, TELEFONE } from '../utils/appImages';
+import './AppFooter.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const AppFooter = () => {
     return (
     <footer>
         <nav className='footer'>
-                {/* <div className='footer-left'>
-
+                <div className='footer-left'>
+                    <p>Avenida Trabalhador São-carlense, 400 - Centro CEP: 13566-590 - São Carlos - SP</p>
                 </div>
 
                 <div className='footer-center' role='navigation'>
                 </div>
 
                 <div className='footer-right'>
-                </div> */}
+                    <div><FontAwesomeIcon icon={faEnvelope} /> crint@icmc.usp.br</div>
+                    <div><FontAwesomeIcon icon={faPhone} /> 3373-8109</div>
+                </div>
             </nav>
     </footer>
     );
---
+++ crint-site/src/pages/wip.tsx
@@ -0,0 +1,10 @@
+import { WIP } from "../utils/appImages";
+
+const WIP_page = () => (
+    <div className="wip">
+        <p>Página em Construção</p>
+        <img src={WIP} />
+    </div>
+);
+
+export default WIP_page;
