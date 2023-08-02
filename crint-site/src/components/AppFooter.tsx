1ed8b0400148f1f9dac7a50781f4566cfdea878e
CRInt-ICMC <136650250+CRInt-ICMC@users.noreply.github.com>
2023-08-02 11:35:31 -0300

    Base criada

--- crint-site/src/components/AppFooter.tsx
+++ crint-site/src/components/AppFooter.tsx
@@ -1,20 +1,25 @@
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { EMAIL, TELEFONE } from '../utils/appImages';
-import './AppFooter.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faTelegram } from '@fortawesome/free-brands-svg-icons';
+import './AppFooter.css'
 
 const AppFooter = () => {
     return (
     <footer>
         <nav className='footer'>
                 <div className='footer-left'>
+                    <h3>Endereço</h3>
                     <p>Avenida Trabalhador São-carlense, 400 - Centro CEP: 13566-590 - São Carlos - SP</p>
                 </div>
 
                 <div className='footer-center' role='navigation'>
+                    <h3>Redes sociais</h3>
+                    <div><FontAwesomeIcon icon={faInstagram} /> crint.icmc</div>
+                    <div><FontAwesomeIcon icon={faTelegram} /> crint.icmc</div>
                 </div>
 
                 <div className='footer-right'>
+                    <h3>Contato</h3>
                     <div><FontAwesomeIcon icon={faEnvelope} /> crint@icmc.usp.br</div>
                     <div><FontAwesomeIcon icon={faPhone} /> 3373-8109</div>
                 </div>
