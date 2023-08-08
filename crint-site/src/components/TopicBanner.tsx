8afef185169b398be9eb6527b71f841a65861859
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2023-08-08 10:50:26 -0300

    Ajuste do Header e dos Banners

--- crint-site/src/components/TopicBanner.tsx
+++ crint-site/src/components/TopicBanner.tsx
@@ -1,16 +1,17 @@
 import './TopicBanner.css';
 import { ICMC_PRETO } from '../utils/appImages';
+import { CSSProperties } from 'react';
 
-const TopicBanner = (props : { topicoNome : string, topicoImage? : string, fontSize? : string}) => {
+const TopicBanner = (props : { topicoNome : string, topicoImage? : string, style? : CSSProperties}) => {
     const defaultImage : string = props.topicoImage || ICMC_PRETO;
-    const defaultFontSize : string = props.fontSize || '6em'
+    const defaultStyle : CSSProperties = props.style || {}
 
 
     return (
         <section className="topic-header">
             <section className='topic-banner'>
                 <img src={defaultImage} alt="" />
-                <h1 style={{fontSize: defaultFontSize}}>{props.topicoNome}</h1>
+                <h1 style={defaultStyle}>{props.topicoNome}</h1>
             </section>
         </section>
     );
