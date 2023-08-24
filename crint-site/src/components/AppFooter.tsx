e8e2192a128aec29722676618e185c8e4c65ebfb
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2023-08-24 07:14:48 -0300

    Globalização do número

--- crint-site/src/components/AppFooter.tsx
+++ crint-site/src/components/AppFooter.tsx
@@ -28,7 +28,7 @@ const AppFooter = () => {
                 <div className='footer-right'>
                     <h3> {langDict.footer.contato} </h3>
                     <a href='mailto:crint@icmc.usp.br'><FontAwesomeIcon icon={faEnvelope} /> crint@icmc.usp.br </a> <br/>
-                    <a href='tel:3373-8109'><FontAwesomeIcon icon={faPhone} /> 3373-8109 </a>
+                    <a href='tel:3373-8109'><FontAwesomeIcon icon={faPhone} /> +55 (16) 3373-8109 </a>
                 </div>
             </nav>
     </footer>
