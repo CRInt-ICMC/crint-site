a2191462589c90f5112fcf3617854cfa229df627
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2023-09-14 08:07:03 -0300

    Limpeza de constantes

--- crint-site/src/paginas/informações paginas/pesquisa.tsx
+++ crint-site/src/paginas/informações paginas/pesquisa.tsx
@@ -1,13 +1,11 @@
 import TopicBanner from '../../componentes/TopicBanner';
 import WIP from '../../componentes/wip';
-import { INFORMACOES_GRADIENTE } from '../../utils/appConstants';
 
 const Pesquisa = () => {
     return (
         <div id='pesquisa-root'>
             <TopicBanner 
                 topicoNome='PESQUISA' 
-                style={{background: INFORMACOES_GRADIENTE}} 
                 />
             <WIP />
         </div>
