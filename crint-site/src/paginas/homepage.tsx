96dbb0c74b212c75ba32951132eea5ff35d583da
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2023-09-21 14:18:18 -0300

    Meio estranho...
    Teste iniciais com o carrossel e alguns ajustes gerais.

--- crint-site/src/paginas/homepage.tsx
+++ crint-site/src/paginas/homepage.tsx
@@ -3,19 +3,47 @@ import { SettingsContext } from '../Contexto';
 import { ApiSecaoSecao } from '../utils/generated/contentTypes';
 import { DEFAULT_LANGUAGE, STRAPI_API_TOKEN, STRAPI_URL } from '../utils/appConstants';
 import TopicSection from '../componentes/TopicSection';
+import Slider from "react-slick";
 import axios from 'axios';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './homepage.scss';
+
+const CreateCarousel = (carouselImages : string[]) => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
+    return (
+        <Slider {...settings}>
+            { 
+                carouselImages.map((image) => {
+
+                    return (
+                        <div className='carousel' key={String(image)}> 
+                            <img src={STRAPI_URL + String(image)} />
+                        </div>
+                    );
+                }) 
+            }
+        </Slider>
+    );
+}
 
 const Homepage = () => {
     const {userSettings} = useContext(SettingsContext);
-    const [backgroundImages, setBackgroundImages] = useState<string>();
+    const [carouselImages, setCarouselImages] = useState<string[]>();
     const [sections, setSections] = useState<ApiSecaoSecao[]>();
 
     // Recebe a imagem de fundo e as seções
     useEffect(() => {
         axios.get(STRAPI_URL + `/api/homepage?populate=*&locale=` + userSettings?.lang || DEFAULT_LANGUAGE, {'headers': {'Authorization': STRAPI_API_TOKEN}})
         .then((response) => {
-            setBackgroundImages(response['data']['data']['attributes']['Imagem_fundo']['data']['attributes']['url']);
+            setCarouselImages([response['data']['data']['attributes']['Imagem_fundo']['data']['attributes']['url']]);
             setSections(response['data']['data']['attributes']['secoes']['data'])
         })
     }, [userSettings?.lang]);
@@ -23,8 +51,9 @@ const Homepage = () => {
     return (
         <div className='homepage-body'>
             {/* Carrega a imagem central */}
-            <section className='main-section' style={{backgroundImage: `url(${STRAPI_URL + backgroundImages})`}}></section>
-
+            <section className='main-section'>
+                { carouselImages && CreateCarousel(carouselImages) }
+            </section>
             {/* Carrega as seções */}
             { sections && 
                 sections.map((section) => {
