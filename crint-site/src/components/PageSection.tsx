908efe8d15efb805ac665871a565207f95f1816c
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2023-11-06 14:17:08 -0300

    Texto não interpola mais com o botão da seção

--- crint-site/src/components/PageSection.tsx
+++ crint-site/src/components/PageSection.tsx
@@ -11,13 +11,13 @@ const PageSection = (props: { id: string, title: string, body: string, textColor
     return (
         <section id={props.id} className='page-section' style={{ color: props.textColor, background: props.backgroundColor }}>
             <div style={{ paddingBottom: collapse ? '0' : '50px' }}>
-                <h1 className='page-title' onClick={() => { setCollapse(!collapse) }}>
-                    {`${props.title}`}
+                <h1 className='section-title' onClick={() => { setCollapse(!collapse) }}>
+                    <div>{`${props.title}`}</div>
                     <button>
                         <FontAwesomeIcon icon={collapse ? faCaretDown : faCaretUp} size='2x' color={props.textColor} />
                     </button>
                 </h1>
-                <AnimateHeight height={collapse ? 0 : 'auto'} duration={500} className='page-content'>
+                <AnimateHeight height={collapse ? 0 : 'auto'} duration={500} className='section-content'>
                     <Interweave content={props.body} allowElements />
                 </AnimateHeight>
             </div>
