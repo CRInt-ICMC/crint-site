78ee4f92304d054da97f6f7ad92665d81f8cfbb5
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2023-12-22 10:27:45 -0300

    Adição da tela de falha de carregamento

--- crint-site/src/components/LoadingScreen.tsx
+++ crint-site/src/components/LoadingScreen.tsx
@@ -2,45 +2,71 @@ import { DotSpinner } from "@uiball/loaders";
 import { useEffect, useState } from "react";
 import { useLoading } from "../utils/utils";
 import './LoadingScreen.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons/faTriangleExclamation";
 
 const LoadingScreen = () => {
     const { loadingCoins } = useLoading();
 
     const [display, setDisplay] = useState(true);
     const [animate, setAnimate] = useState(false);
+    const [failed, setFailed] = useState(false);
+    const [timeoutClock, setTimeoutClock] = useState<ReturnType<typeof setTimeout>>();
 
-    // Tempo de transição e timeout da tela de carregamento
-    const time = 700;
+    // Tempo de transição da tela de carregamento
+    const animationTime = 700;
+    
+    const timeoutTime = 10000;
+
+    // Trava local para evitar que o timeout seja setado mais de uma vez
+    let t: ReturnType<typeof setTimeout>;
 
     useEffect(() => {
         if (loadingCoins <= 0) {
             setAnimate(true);
-            setTimeout(() => setDisplay(false), time);
+            setTimeout(() => setDisplay(false), animationTime); // Não sai da tela de carregamento até que a animação termine
+
+            if (timeoutClock !== undefined) {
+                clearTimeout(timeoutClock);
+                setTimeoutClock(undefined);
+            }
         }
 
         else {
             setAnimate(false);
             setDisplay(true);
+
+            if (timeoutClock === undefined && t === undefined) {
+                t = setTimeout(() => setFailed(true), timeoutTime)
+                setTimeoutClock(t);
+            }
         }
     }, [loadingCoins]);
 
-    return (
-        <div
-            className={'loader-root ' + (animate ? 'hidden' : 'showing')}
-            style={{
-                display: display ? 'flex' : 'none',
-                transitionDuration: String(time) + 'ms',
-            }}
-        >
+    return <>
+        {!failed
+            ? < div
+                className={'loader-root ' + (animate ? 'hidden' : 'showing')}
+                style={{
+                    display: display ? 'flex' : 'none',
+                    transitionDuration: String(animationTime) + 'ms',
+                }}
+            >
 
-            <DotSpinner
-                size={40}
-                speed={0.9}
-                color="black"
-            />
-            <span className='loader-text'>Requisitando conteúdo</span>
-        </div>
-    );
+                <DotSpinner
+                    size={40}
+                    speed={0.9}
+                    color="black"
+                />
+                <span className='loader-text'>Requisitando conteúdo</span>
+            </div >
+
+            : <div className='loader-root failure'> 
+                <FontAwesomeIcon icon={faTriangleExclamation} size='4x' color='red' />
+                <span className='loader-text'>Ops... Parece que estamos sem conexão com o servidor</span>
+            </div>
+        }
+    </>
 }
 
 export default LoadingScreen;
