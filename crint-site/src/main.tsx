59022fed07d180cd5db4ed34f21942747072130b
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2024-01-03 08:49:26 -0300

    Adição da licensa GPLv3

--- crint-site/src/main.tsx
+++ crint-site/src/main.tsx
@@ -1,3 +1,18 @@
+// This file is part of CRInt-site.
+
+// CRInt-site is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// CRInt-site is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with CRInt-site. If not, see <https://www.gnu.org/licenses/>.
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
