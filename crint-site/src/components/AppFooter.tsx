085d65b7230372e627081a7537f1b7b874d850db
KrulesExGamer <57680132+PedroVNasc@users.noreply.github.com>
2024-07-22 11:11:06 -0300

    Header e Footer refatorados

--- crint-site/src/components/AppFooter.tsx
+++ crint-site/src/components/AppFooter.tsx
@@ -19,28 +19,37 @@ import { IconName, IconPrefix } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
 import { STRAPI_API_TOKEN, STRAPI_URL } from '../utils/constants';
 import { useLoading, useSettings } from '../utils/utils';
-import { ApiFooter, ApiLink } from '../utils/types';
+import { ApiLink } from '../utils/types';
 import { readCache, setCache } from '../Caching';
 import { Grid } from '@mui/material';
 import axios from 'axios';
 import './AppFooter.scss'
 
+interface FooterLink {
+    text: string,
+    url: string,
+    icon: [IconPrefix, IconName],
+}
+
+interface FooterData {
+    address: { title: string, text: string },
+    socials: { title: string, links: FooterLink[] },
+    contacts: { title: string, links: FooterLink[] },
+    credits: string,
+    privacy: string,
+}
+
 const AppFooter = () => {
     const { userSettings } = useSettings();
     const { addLoadingCoins, subLoadingCoins } = useLoading();
-    const [footerData, setFooterData] = useState<ApiFooter>();
-    const [linksContato, setLinksContato] = useState<ApiLink[]>();
-    const [linksRedesSociais, setLinksRedesSociais] = useState<ApiLink[]>();
+    const [footerData, setFooterData] = useState<FooterData>();
 
     // Executa apenas uma vez quando o site é carregado
     useEffect(() => {
         const cacheFooter = readCache('footer' + '-' + userSettings.lang);
 
-        if (cacheFooter) {
+        if (cacheFooter)
             setFooterData(cacheFooter);
-            setLinksContato(cacheFooter['attributes']['Links_contatos']['data'] as ApiLink[]);
-            setLinksRedesSociais(cacheFooter['attributes']['Links_redes']['data'] as ApiLink[]);
-        }
 
         else {
             addLoadingCoins();
@@ -49,12 +58,42 @@ const AppFooter = () => {
                 .get(STRAPI_URL + '/api/footer?populate[Links_contatos][populate][0]=Icone_FA&populate[Links_redes][populate][0]=Icone_FA&locale=' + userSettings.lang,
                     { 'headers': { 'Authorization': STRAPI_API_TOKEN } })
                 .then((response) => {
-                    const data = response['data']['data'];
+                    const raw = response['data']['data'];
 
-                    setFooterData(data as ApiFooter);
-                    setLinksContato(data['attributes']['Links_contatos']['data'] as ApiLink[]);
-                    setLinksRedesSociais(data['attributes']['Links_redes']['data'] as ApiLink[]);
+                    let data: FooterData = {
+                        address: {
+                            title: raw['attributes']['Endereco_titulo'],
+                            text: raw['attributes']['Endereco_texto']
+                        },
+                        socials: {
+                            title: raw['attributes']['Redes_sociais'],
+                            links: []
+                        },
+                        contacts: {
+                            title: raw['attributes']['Contato_titulo'],
+                            links: []
+                        },
+                        credits: raw['attributes']['Creditos'],
+                        privacy: raw['attributes']['Politica_privacidade']
+                    }
 
+                    raw['attributes']['Links_contatos']['data'].map((link: ApiLink) => {
+                        data['contacts']['links'].push({
+                            text: String(link['attributes']['Texto']),
+                            url: String(link['attributes']['Link']),
+                            icon: String(link['attributes']['Icone_FA']).split(',') as [IconPrefix, IconName]
+                        })
+                    })
+
+                    raw['attributes']['Links_redes']['data'].map((link: ApiLink) => {
+                        data['socials']['links'].push({
+                            text: String(link['attributes']['Texto']),
+                            url: String(link['attributes']['Link']),
+                            icon: String(link['attributes']['Icone_FA']).split(',') as [IconPrefix, IconName]
+                        })
+                    })
+
+                    setFooterData(data);
                     setCache('footer' + '-' + userSettings.lang, data);
 
                     subLoadingCoins();
@@ -69,20 +108,18 @@ const AppFooter = () => {
                     <Grid sx={{ rowGap: 8 }} container spacing={0} justifyContent="space-evenly" alignItems="flex-start" className='footer-row'>
                         {/* ENDEREÇO */}
                         <Grid item xs={8} md={3} className='footer-column' style={{ width: '25%' }}>
-                            <h3> {String(footerData.attributes.Endereco_titulo)} </h3>
-                            <p className='text'> {String(footerData.attributes.Endereco_texto)} </p>
+                            <h3> {footerData.address.title} </h3>
+                            <p className='text'> {footerData.address.text} </p>
                         </Grid>
 
                         {/* REDES SOCIAIS */}
                         <Grid item xs={5.5} md={2.5} className='footer-column' role='navigation' style={{ width: '30%', padding: 0, textAlign: 'center' }}>
-                            <h3> {String(footerData.attributes.Redes_sociais)} </h3>
-                            {linksRedesSociais && linksRedesSociais.map((link) => {
-                                const icon = String(link.attributes.Icone_FA).split(',') as [IconPrefix, IconName];
-
-                                return <div className='text' key={String(link.attributes.Texto)}>
-                                    <a href={String(link.attributes.Link)}>
-                                        <FontAwesomeIcon icon={icon} />
-                                        {' ' + String(link.attributes.Texto)}
+                            <h3> {footerData.socials.title} </h3>
+                            {footerData.socials && footerData.socials.links.map((link) => {
+                                return <div className='text' key={link.text}>
+                                    <a href={link.url}>
+                                        <FontAwesomeIcon icon={link.icon} />
+                                        {' ' + link.text}
                                     </a>
                                 </div>
                             })}
@@ -90,14 +127,12 @@ const AppFooter = () => {
 
                         {/* CONTATO */}
                         <Grid item xs={5.5} md={3} className='footer-column' role='navigation' style={{ width: '25%' }}>
-                            <h3> {String(footerData.attributes.Contato_titulo)} </h3>
-                            {linksContato && linksContato.map((link) => {
-                                const icon = String(link.attributes.Icone_FA).split(',') as [IconPrefix, IconName];
-
-                                return <div className='text' key={String(link.attributes.Texto)}>
-                                    <a href={String(link.attributes.Link)}>
-                                        <FontAwesomeIcon icon={icon} />
-                                        {' ' + String(link.attributes.Texto)}
+                            <h3> {footerData.contacts.title} </h3>
+                            {footerData.contacts && footerData.contacts.links.map((link) => {
+                                return <div className='text' key={link.text}>
+                                    <a href={link.url}>
+                                        <FontAwesomeIcon icon={link.icon} />
+                                        {' ' + link.text}
                                     </a>
                                 </div>
                             })}
@@ -106,10 +141,10 @@ const AppFooter = () => {
 
                     <Grid sx={{ rowGap: 2, columnGap: 16 }} container justifyContent="center" className='footer-row'>
                         <Grid item className='footer-column' xs={10} md={4} style={{ textAlign: 'center' }}>
-                            <Link to={'/creditos'}>{String(footerData.attributes.Creditos)}</Link >
+                            <Link to={'/creditos'}>{footerData.credits}</Link >
                         </Grid>
                         <Grid item className='footer-column' xs={10} md={4} style={{ textAlign: 'center' }}>
-                            <Link to={'/privacidade'}>{String(footerData.attributes.Politica_privacidade)}</Link >
+                            <Link to={'/privacidade'}>{footerData.privacy}</Link >
                         </Grid>
                     </Grid>
                 </nav>
--- crint-site/src/components/AppHeader.tsx
+++ crint-site/src/components/AppHeader.tsx
@@ -30,20 +30,34 @@ import Grid from '@mui/material/Grid';
 import AnimateHeight from 'react-animate-height';
 import './AppHeader.scss';
 
-const topics = (topicos: ApiTopic[]) => (
+interface HeaderTopics {
+    name: string,
+    pages: {
+        title: string,
+        url: string,
+    }[],
+}
+
+interface HeaderImages {
+    icmc: string,
+    icmcMini: string,
+}
+
+
+const topics = (topicos: HeaderTopics[]) => (
     <Grid item xs={5} md={8} className='navbar-column navbar-center' role='navigation'>
         {
             topicos.map((topico) => (<DropdownMenu
-                key={String(topico.attributes.Nome)}
-                head={<p>{String(topico.attributes.Nome)}</p>}
+                key={topico.name}
+                head={<p>{topico.name}</p>}
                 body={<span className='subtopics'>
                     {
-                        (topico.attributes.paginas as any)['data'].map((pagina: ApiPage) => (
+                        topico.pages.map((page: { title: string, url: string }) => (
                             <Link
-                                key={String(pagina.attributes.Titulo)}
-                                to={String(pagina.attributes.URL)}
+                                key={page.title}
+                                to={page.url}
                             >
-                                {String(pagina.attributes.Titulo)}
+                                {page.title}
                             </Link>
                         ))
                     }
@@ -55,7 +69,8 @@ const topics = (topicos: ApiTopic[]) => (
     </Grid>
 )
 
-const topicsMobile = (topicos: ApiTopic[], currentUrl: string, open: Boolean, toggleOpen: CallableFunction) => (
+
+const topicsMobile = (topicos: HeaderTopics[], currentUrl: string, open: Boolean, toggleOpen: CallableFunction) => (
     <>
         <Grid item xs={5} md={8.5} className='navbar-column navbar-center' role='navigation'>
             <div className='navbar-mobile'>
@@ -72,17 +87,17 @@ const topicsMobile = (topicos: ApiTopic[], currentUrl: string, open: Boolean, to
             <Grid justifyContent="center" container>
                 {
                     topicos.map((topico) => (
-                        <Grid xs={8} key={String(topico.attributes.Nome)}>
+                        <Grid item xs={8} key={topico.name}>
                             <span className='subtopics'>
-                                <span className='title'>{String(topico.attributes.Nome)}</span>
+                                <span className='title'>{topico.name}</span>
                                 {
-                                    (topico.attributes.paginas as any)['data'].map((pagina: ApiPage) => (
-                                        <Link className={(currentUrl === String(pagina.attributes.URL)) ? 'highlight' : ''}
-                                            key={String(pagina.attributes.Titulo)}
-                                            to={String(pagina.attributes.URL)}
+                                    topico.pages.map((pagina: { title: string, url: string }) => (
+                                        <Link className={(currentUrl === pagina.url) ? 'highlight' : ''}
+                                            key={pagina.title}
+                                            to={pagina.url}
                                         >
                                             <FontAwesomeIcon icon={faAngleRight} style={{ paddingRight: '5px' }} />
-                                            {String(pagina.attributes.Titulo)}
+                                            {pagina.title}
                                         </Link>
                                     ))
                                 }
@@ -95,10 +110,6 @@ const topicsMobile = (topicos: ApiTopic[], currentUrl: string, open: Boolean, to
     </>
 )
 
-interface HeaderImages {
-    icmc: StrapiImageData,
-    icmcMini: StrapiImageData,
-}
 
 const AppHeader = () => {
     // Hooks    
@@ -107,18 +118,15 @@ const AppHeader = () => {
     const { addLoadingCoins, subLoadingCoins } = useLoading();
 
     const [headerImages, setHeaderImages] = useState<HeaderImages>();
-    const [topicos, setTopicos] = useState<ApiTopic[]>();
+    const [topicos, setTopicos] = useState<HeaderTopics[]>();
     const [open, toggleOpen] = useState(false);
 
     const mobile = useMediaPredicate("(orientation: portrait)");
     const location = useLocation();
 
-    console.log(open)
-
     // Executa apenas quando a linguagem é alterada
     useEffect(() => {
         const cacheHeaderImages = readCache('headerImages');
-        // const cachePopupText = readCache('popup' + '-' + userSettings.lang);
         const cacheTopicos = readCache('topicos' + '-' + userSettings.lang);
 
         if (cacheHeaderImages)
@@ -133,8 +141,8 @@ const AppHeader = () => {
                     const data = response['data']['data'];
 
                     const dataImages = {
-                        icmc: data['attributes']['ICMC']['data']['attributes'] as StrapiImageData,
-                        icmcMini: data['attributes']['ICMC_mini']['data']['attributes'] as StrapiImageData,
+                        icmc: data['attributes']['ICMC']['data']['attributes']['url'],
+                        icmcMini: data['attributes']['ICMC_mini']['data']['attributes']['url'],
                     };
 
                     setHeaderImages(dataImages);
@@ -152,8 +160,21 @@ const AppHeader = () => {
             axios
                 .get(STRAPI_URL + '/api/topicos?populate=*&locale=' + userSettings.lang, { 'headers': { 'Authorization': STRAPI_API_TOKEN } })
                 .then((response) => {
-                    const dataTopicos: ApiTopic[] = [];
-                    response['data']['data'].map((topico: ApiTopic) => {
+                    const dataTopicos: HeaderTopics[] = [];
+                    response['data']['data'].map((raw: ApiTopic) => {
+                        let topico: HeaderTopics = {
+                            name: String(raw['attributes']['Nome']),
+                            pages: [],
+                        };
+
+
+                        raw['attributes']['paginas']['data'].map((page: ApiPage) => {
+                            topico.pages.push({
+                                title: String(page['attributes']['Titulo']),
+                                url: String(page['attributes']['URL']),
+                            })
+                        });
+
                         dataTopicos.push(topico);
                     })
 
@@ -170,7 +191,10 @@ const AppHeader = () => {
                 {/* LOGO */}
                 <Grid item xs={3} md={2} className='navbar-column logo'>
                     {headerImages &&
-                        <Link to={'/'}><img className='logo-crint' alt='Link Página Principal' src={STRAPI_URL + (mobile ? headerImages.icmcMini.url : headerImages.icmc.url)} /></Link>
+                        <Link to={'/'}>
+                            <img className='logo-crint' alt='Link Página Principal' src={STRAPI_URL +
+                                (mobile ? headerImages.icmcMini : headerImages.icmc)} />
+                        </Link>
                     }
                 </Grid>
 
